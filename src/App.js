71fa997f77f1d1fd29c4ecc8e622d67e2a3f7743
Nadav Kaner <nadavkaner@invisionapp.com>
2021-02-16 23:42:19 +0200

    Add different color representations

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useEffect, useCallback, useState } from "react";
+import tinycolor from 'tinycolor2';
 import "./App.css";
 
 const PLUGIN_MESSAGE_API = {
-  initialized: "initialized"
+  initialized: "initialized",
 };
 
 function App() {
@@ -28,20 +29,58 @@ function App() {
   return (
     <div>
       {(styleguide?.colors || []).map((color) => {
+        const colorObj = tinycolor(color.value);
         return (
-          <div style={{ display: "flex" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              borderBottom: "1px solid #EBECEE",
+              marginBottom: 16,
+              padding: 16,
+            }}
+          >
             <div
               style={{
-                width: 50,
-                height: 50,
+                width: "100%",
+                height: 212,
                 backgroundColor: color.value,
                 marginRight: 16,
                 borderRadius: 4,
-                marginBottom: 8,
-                boxShadow: 'inset 0 0 0 1px rgb(29 29 31 / 5%)'
+                marginBottom: 24,
+                boxShadow: "inset 0 0 0 1px rgb(29 29 31 / 5%)",
               }}
             />
-            <div style={{ display: 'flex', alignItems: 'center' }}>{color.name}</div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                fontFamily: "Eina 03",
+                fontSize: 18,
+                fontWeight: 600,
+                marginBottom: 24
+              }}
+            >
+              {color.name}
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <div style={{ display: 'flex' }}>
+                <div style={{ width: 80, color: '#6E7077', marginBottom: 8 }}>HEX</div>
+                <div>{colorObj.toHexString().toUpperCase()}</div>
+              </div>
+              <div style={{ display: 'flex' }}>
+                <div style={{ width: 80, color: '#6E7077', marginBottom: 8 }}>RGBA</div>
+                <div>{colorObj.toRgbString()}</div>
+              </div>
+              <div style={{ display: 'flex' }}>
+                <div style={{ width: 80, color: '#6E7077', marginBottom: 8 }}>HSL</div>
+                <div>{colorObj.toHslString()}</div>
+              </div>
+              <div style={{ display: 'flex' }}>
+                <div style={{ width: 80, color: '#6E7077', marginBottom: 8 }}>HSV</div>
+                <div>{colorObj.toHsvString()}</div>
+              </div>
+            </div>
           </div>
         );
       })}
